689154f29ab668d47499499e621360053b008707
aqqzzz <592710057@qq.com>
2017-06-14 20:07:47 +0800

    update

--- 作业/作业10/StartFromWsdl/Assignment7/ServerSide2/src/test/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- *         student score management web service description
- *     
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://jw.nju.edu.cn/schema", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package test;
